4eb3fabc181b70cd55499e0494a303fd5b55e17a
bytebutcher <noname@bytebutcher.net>
2026-02-22 22:27:47 +0100

    version 2.3.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ extra_files = package_files('src/snippet/.snippet')
 # This call to setup() does all the work
 setup(
     name="snippet-cli",
-    version="2.2.0",
+    version="2.3.0",
     description="An advanced snippet manager for the command-line.",
     long_description=README,
     long_description_content_type="text/markdown",
